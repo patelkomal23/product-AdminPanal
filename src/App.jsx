da8aab4213b3d204323749dfff8770667a73f67d
patelkomal23 <patelkomal2332@gmail.com>
2025-06-09 17:51:49 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,168 @@
+
+import React, { useEffect, useState, useRef } from "react";
+import Form from "./Pages/Form";
+import Datatable from "./Pages/Datatable";
+import Home from "./Pages/Home";
+import axios from "axios";
+import { Route, Routes, useNavigate } from "react-router-dom";
+
+
+const App = () => {
+  const [product, setProduct] = useState({});
+  const [productsData, setProductsData] = useState([]);
+  const [godown, setGodown] = useState([]);
+  const [editId, setEditId] = useState("");
+  const imgRef = useRef();
+  const [error, setError] = useState({});
+
+  const navigate = useNavigate();
+  const URL = "http://localhost:3000/product";
+
+  // useEffect(() => {
+  //   const oldData = JSON.parse(localStorage.getItem("productss") || "[]");
+  //   setProductsData(oldData);
+  // }, []);
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+ const handleFetch = async () => {
+  try {
+    const res = await axios.get(URL);
+    setProductsData(Array.isArray(res.data) ? res.data : []);
+    setEditId("");
+    setProduct({});
+  } catch (err) {
+    console.error("Fetch error:", err);
+  }
+};
+
+  const handleChange = (e) => {
+    const { name, value, type, checked, files } = e.target;
+ if (type === "file") {
+      const file = files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const product_image = {
+            name: file.name,
+            type: file.type,
+            url: reader.result,
+          };
+          setProduct((prev) => ({ ...prev, product_image }));
+        };
+        reader.readAsDataURL(file);
+      }
+    } else {
+      setProduct((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
+  const validation = () => {
+    let errors = {};
+    if (!product.product_name)
+      errors.product_name = "*Product Name is required";
+    if (!product.product_price)
+      errors.product_price = "*Product Price is required";
+    if (!product.product_stock) errors.product_stock = "*Stock is required";
+   
+    if (!product.description) errors.description = "*Description is required";
+
+    setError(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validation()) return;
+
+    if (editId === "") {
+      await axios.post(URL, { ...product,id: String(Date.now()) });
+    } else {
+      let res = await axios.put(`${URL}/${editId}`, { ...product });
+    }
+    handleFetch();
+
+    setProduct({});
+    imgRef.current.value = "";
+
+    navigate("/datatable");
+  };
+
+  const handleDelete = async (id) => {
+    await axios.delete(`${URL}/${id}`);
+    handleFetch();
+  };
+
+  const handleEdit = (id) => {
+    let user = productsData.find((item) => item.id === id);
+    if (!user) return;
+
+    setEditId(id);
+    setProduct(user);
+    navigate("/form");
+  };
+
+  return (
+    <>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/form"
+          element={
+            <Form
+              handleChange={handleChange}
+              product={product}
+              handleSubmit={handleSubmit}
+              imgRef={imgRef}
+              isEdit={editId !== ""}
+              error={error}
+            />
+          }
+        />
+        <Route
+          path="/datatable"
+          element={
+            <Datatable
+              productsData={productsData}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          }
+        />
+      </Routes> */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+
+        <Route
+          path="/form"
+          element={
+            <Form
+              handleChange={handleChange}
+              product={product}
+              handleSubmit={handleSubmit}
+              imgRef={imgRef}
+              isEdit={editId !== ""}
+              error={error}
+            />
+          }
+        />
+        <Route
+        path="/datatable"
+        element={<Datatable
+        productsData={productsData}
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
+        />
+      }
+        
+        />
+
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Pages/Form.jsx
@@ -0,0 +1,416 @@
+
+import React from "react";
+
+import Header from "../components/Header";
+import Side from "../components/Side"
+
+const Form = ({
+  handleChange,
+  product,
+
+  handleSubmit,
+  imgRef,
+  error,
+}) => {
+  return (
+    <>
+      <div className="wrapper">
+        {/* Sidebar */}
+       <Side/>
+        {/* End Sidebar */}
+        <div className="main-panel">
+          {/* Navbar start*/}
+          <Header />
+          {/* Navbar end */}
+          <div className="container">
+            <div className="page-inner">
+              <div className="page-header">
+                <h3 className="fw-bold mb-3">Forms</h3>
+                <ul className="breadcrumbs mb-3">
+                  <li className="nav-home">
+                    <a href="#">
+                      <i className="icon-home" />
+                    </a>
+                  </li>
+                  <li className="separator">
+                    <i className="icon-arrow-right" />
+                  </li>
+                  <li className="nav-item">
+                    <a href="#">Forms</a>
+                  </li>
+                  <li className="separator">
+                    <i className="icon-arrow-right" />
+                  </li>
+                  <li className="nav-item">
+                    <a href="#">Basic Form</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <form action="" method="post" onSubmit={handleSubmit}>
+                    <div className="card">
+                      <div className="card-header">
+                        <div className="card-title">Form Elements</div>
+                      </div>
+                      <div className="card-body">
+                        <div className="row">
+                          <div className="col-md-6 col-lg-4">
+                            {/* Product-Name */}
+                            <div className="form-group">
+                              <label htmlFor="product_name" className="fw-bold">
+                                Product Name{" "}
+                              </label>
+                              <input
+                                onChange={handleChange}
+                                name="product_name"
+                                value={product.product_name || ""}
+                                type="text"
+                                className="form-control"
+                                id="product_name"
+                                placeholder="Enter product name"
+                              />
+                              {error.product_name && (
+                                <span className="text-danger">
+                                  {error.product_name}
+                                </span>
+                              )}
+                            </div>
+                            <div className="form-group">
+                              <label
+                                htmlFor="product_price"
+                                className="fw-bold"
+                              >
+                                Product Price{" "}
+                              </label>
+                              <input
+                                type="text"
+                                name="product_price"
+                                onChange={handleChange}
+                                value={product.product_price || ""}
+                                className="form-control"
+                                id="product_price"
+                                placeholder="Enter product price"
+                              />
+                              {error.product_price && (
+                                <span className="text-danger">
+                                  {error.product_price}
+                                </span>
+                              )}
+                            </div>
+                            {/*Available stock*/}
+                            <div className="form-group">
+                              <label
+                                htmlFor="product_stock"
+                                className="fw-bold"
+                              >
+                                Product Stock
+                              </label>
+                              <input
+                                type="text"
+                                name="product_stock"
+                                onChange={handleChange}
+                                value={product.product_stock || ""}
+                                className="form-control"
+                                id="product_stock"
+                                placeholder="Enter Stock available"
+                              />
+                              {error.product_stock && (
+                                <span className="text-danger">
+                                  {error.product_stock}
+                                </span>
+                              )}
+                            </div>
+                            {/*Image */}
+                            <div className="form-group">
+                              <label
+                                htmlFor="product_Image"
+                                className="fw-bold"
+                              >
+                                Product Image :
+                              </label>
+                              <input
+                                type="file"
+                                ref={imgRef}
+                                name="file"
+                                onChange={handleChange}
+                                className="form-control"
+                                id="product_image"
+                                placeholder="Enter product image"
+                              />
+                              {/* {error.file && (
+                                <span className="text-danger">
+                                  {error.file}
+                                </span>
+                              )} */}
+                            </div>
+                         
+
+                            <div className="form-group">
+                              <label htmlFor="description" className="fw-bold">
+                                Description{" "}
+                              </label>
+                              <textarea
+                                onChange={handleChange}
+                                name="description"
+                                value={product.description || ""}
+                                className="form-control"
+                                id="description"
+                              />
+                              {error.description && (
+                                <span className="text-danger">
+                                  {error.description}
+                                </span>
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="card-action">
+                        <button className="btn btn-success">Submit</button>
+                        <button className="btn btn-danger">Cancel</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="container-fluid d-flex justify-content-between">
+              <nav className="pull-left">
+                <ul className="nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="http://www.themekita.com">
+                      ThemeKita
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      {" "}
+                      Help{" "}
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      {" "}
+                      Licenses{" "}
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="copyright">
+                2024, made with <i className="fa fa-heart heart text-danger" />{" "}
+                by
+                <a href="http://www.themekita.com">ThemeKita</a>
+              </div>
+              <div>
+                Distributed by
+                <a target="_blank" href="https://themewagon.com/">
+                  ThemeWagon
+                </a>
+                .
+              </div>
+            </div>
+          </footer>
+        </div>
+        {/* Custom template | don't include it in your project! */}
+        <div className="custom-template">
+          <div className="title">Settings</div>
+          <div className="custom-content">
+            <div className="switcher">
+              <div className="switch-block">
+                <h4>Logo Header</h4>
+                <div className="btnSwitch">
+                  <button
+                    type="button"
+                    className="selected changeLogoHeaderColor"
+                    data-color="dark"
+                  />
+                  <button
+                    type="button"
+                    className="selected changeLogoHeaderColor"
+                    data-color="blue"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="purple"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="light-blue"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="green"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="orange"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="red"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="white"
+                  />
+                  <br />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="dark2"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="blue2"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="purple2"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="light-blue2"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="green2"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="orange2"
+                  />
+                  <button
+                    type="button"
+                    className="changeLogoHeaderColor"
+                    data-color="red2"
+                  />
+                </div>
+              </div>
+              <div className="switch-block">
+                <h4>Navbar Header</h4>
+                <div className="btnSwitch">
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="dark"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="blue"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="purple"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="light-blue"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="green"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="orange"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="red"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="white"
+                  />
+                  <br />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="dark2"
+                  />
+                  <button
+                    type="button"
+                    className="selected changeTopBarColor"
+                    data-color="blue2"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="purple2"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="light-blue2"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="green2"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="orange2"
+                  />
+                  <button
+                    type="button"
+                    className="changeTopBarColor"
+                    data-color="red2"
+                  />
+                </div>
+              </div>
+              <div className="switch-block">
+                <h4>Sidebar</h4>
+                <div className="btnSwitch">
+                  <button
+                    type="button"
+                    className="selected changeSideBarColor"
+                    data-color="white"
+                  />
+                  <button
+                    type="button"
+                    className="changeSideBarColor"
+                    data-color="dark"
+                  />
+                  <button
+                    type="button"
+                    className="changeSideBarColor"
+                    data-color="dark2"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="custom-toggle">
+            <i className="icon-settings" />
+          </div>
+        </div>
+        {/* End Custom template */}
+      </div>
+    </>
+  );
+};
+
+export default Form;
